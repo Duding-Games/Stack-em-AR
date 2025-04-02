a63b3c09e56e93f1dc22bdf8986eaca3e089a1fa
eduardobr33 <100405421+eduardobr33@users.noreply.github.com>
2025-04-02 18:54:38 +0200

    GameManager
    - Added a GameManager to manage the scenes.
    - Added a DestroyObject script to manage objects that shouldn't be on the scene.

---
+++ Stack'em/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float gameDuration = 60f;
+    private float timeRemaining;
+    private bool gameActive = true;
+
+    public Text timerText;
+
+    void Start()
+    {
+        timeRemaining = gameDuration;
+    }
+
+    void Update()
+    {
+        if (gameActive)
+        {
+            timeRemaining -= Time.deltaTime;
+            UpdateTimerUI();
+
+            if (timeRemaining <= 0)
+            {
+                EndGame();
+            }
+        }
+    }
+
+    void UpdateTimerUI()
+    {
+        if (timerText != null)
+        {
+            timerText.text = "Time: " + Mathf.Ceil(timeRemaining).ToString();
+        }
+    }
+
+    void EndGame()
+    {
+        gameActive = false;
+        Debug.Log("Game Over!");
+    }
+}
